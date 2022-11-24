3288b6dbf49dd495b095d0b065a18080af1d598b
SetsunaKissshot <416772623@qq.com>
2022-11-24 20:31:42 +0800

    Add alien invasion project

---
+++ alien_invasion/ship.py
@@ -0,0 +1,19 @@
+import pygame
+
+class Ship:
+    def __init__(self, ai_game):
+        '''Settings about ship'''
+        # Initial ship and set location
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Load ship image and get circumscribed rectangle shape
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        # Put ship at the bottom of screen
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        '''Generate ship on screen'''
+        self.screen.blit(self.image, self.rect)
