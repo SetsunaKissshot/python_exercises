c7d935a1821ad5a65529f5fcd772be4b2241a490
SetsunaKissshot <416772623@qq.com>
2022-11-26 17:52:03 +0800

    Add implementation of ship movement

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -20,16 +20,35 @@ class AlienInvasion:
         self.ship = Ship(self)
 
     def run_game(self):
+        '''Start game'''
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
 
-            self.screen.fill(self.settings.bg_color)  # Set background color
-            self.ship.blitme()
+    def _check_events(self):
+        '''Response to mouse and keyboard'''
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    '''Ship moves to right'''
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    '''Ship moves to left'''
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
-            # Regenerate screen every loop
-            pygame.display.flip()
+    def _update_screen(self):
+        '''Update contents on screen'''
+        self.screen.fill(self.settings.bg_color)  # Set background color
+        self.ship.blitme()  # Generate ship
+        pygame.display.flip()  # Regenerate screen
 
 
 if __name__ == '__main__':
--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -7,14 +7,29 @@ class Ship:
         # Initial ship and set location
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         # Load ship image and get circumscribed rectangle shape
-        self.image = pygame.image.load(
-            '/home/setsuna/workspace/python_exercises/alien_invasion/images/ship.bmp')
+        self.image = pygame.image.load('./images/ship.bmp')
         self.rect = self.image.get_rect()
 
         # Put ship at the bottom of screen
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Set ship speed
+        self.x = float(self.rect.x)
+
+        # Move to right
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        elif self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        self.rect.x = self.x
 
     def blitme(self):
         '''Generate ship on screen'''
